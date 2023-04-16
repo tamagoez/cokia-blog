eec07b1479acc124a11b8b877a0105edba88ffca
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-04-16 10:07:32 +0900

    Edit some pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Cokia Pages</span>,
   project: {
     link: 'https://github.com/tamagoez/cokia-web',
   },
