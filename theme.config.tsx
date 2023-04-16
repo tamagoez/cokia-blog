2263ae3990311c7f918ae19d3a7f07ee393e7066
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-04-17 00:28:39 +0900

    Change titleTemplate

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
   logo: <span>Neem Pages</span>,
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s – Neem Pages",
+      };
+    }
+  },
   project: {
-    link: 'https://github.com/tamagoez/neem-web',
+    link: "https://github.com/tamagoez/neem-web",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/tamagoez/neem-pages',
+  docsRepositoryBase: "https://github.com/tamagoez/neem-pages",
   footer: {
-    text: 'Neem Pages (written by @tamagoez)',
+    text: "Neem Pages (written by @tamagoez)",
   },
-}
+};
 
 export default config
