6a511af741155a2bbcd9592ff71c33e5482e688c
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-04-20 09:40:42 +0900

    Duplicate `import`

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-import { useRouter } from "next/router";
 import { useRouter } from 'next/router'
 import { useConfig } from 'nextra-theme-docs'
 
