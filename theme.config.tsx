afcada5bdaf4f60210a13e8ca6d03cdc489e54c6
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-04-20 09:36:59 +0900

    Add banner, head, edit on GitHub(temp)

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,25 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://my-app.com' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return <>
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={frontMatter.title || 'Neem Pages'} />
+      <meta property="og:description" content={frontMatter.description || 'Writting about confidential matters'} />
+    </>
+  },
+  banner: {
+    key: '0.0.1-release',
+    text: <a href="https://nextra.site" target="_blank">
+      ⛏ Neem 0.0.1 will be released. Join Beta →
+    </a>,
+  },
   logo: <span>Neem Pages</span>,
   useNextSeoProps() {
     const { asPath } = useRouter();
@@ -22,6 +41,10 @@ const config: DocsThemeConfig = {
   footer: {
     text: "Neem Pages (written by @tamagoez)",
   },
+  editLink: {
+    text: "Edit on GitHub"
+  },
+  faviconGlyph: "📝"
 };
 
 export default config
